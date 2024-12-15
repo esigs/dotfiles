18e5071100d453e4250c694c64289ad920369cec
Eric Sigurdson <eric@ericsigurdson.com>
2024-12-15 16:23:12 -0500

    config continued

---
+++ nvim/.config/nvim/lua/eric/core.lua
@@ -0,0 +1,17 @@
+vim.wo.number = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = true
+
+vim.opt.updatetime= 40
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.clipboard = "unnamedplus"
---
+++ nvim/.config/nvim/lua/eric/init.lua
@@ -0,0 +1,2 @@
+require("eric.remap")
+require("eric.core")
