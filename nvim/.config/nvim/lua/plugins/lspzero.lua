0ca00b04a4a95820bbc865b526b620b5536ef3a9
Eric Sigurdson <eric@ericsigurdson.com>
2025-01-25 11:03:04 -0500

    lsp added

---
+++ nvim/.config/nvim/lua/plugins/lspzero.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      -- Reserve space in the gutter
+      vim.opt.signcolumn = 'yes'
+
+      -- Add cmp_nvim_lsp capabilities settings to lspconfig
+      local lspconfig_defaults = require('lspconfig').util.default_config
+      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+
+      -- LSP actions on attach
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+          vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+        end,
+      })
+
+      -- Example language servers
+      require('lspconfig').gleam.setup({})
+      require('lspconfig').ocamllsp.setup({})
+      require('lspconfig').csharp_ls.setup{}
+    end,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      local cmp = require('cmp')
+
+      cmp.setup({
+        sources = {
+          { name = 'nvim_lsp' },
+        },
+        snippet = {
+          expand = function(args)
+            -- You need Neovim v0.10+ to use vim.snippet
+            vim.snippet.expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({}),
+      })
+    end,
+  },
+}
