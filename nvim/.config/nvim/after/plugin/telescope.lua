05e2cbd3f204eec1f3c2fb7ddcd9ad8ac03fe5d9
Eric Sigurdson <eric@ericsigurdson.com>
2025-04-26 12:12:55 -0400

    Stowing emacs config.
    Including previously updated telescope binding to search open buffers.

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
     
     vim.keymap.set('n', '<leader>ph', function()
         builtin.find_files({ cwd = vim.fn.expand("~") })
