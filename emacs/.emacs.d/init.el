a436dbebfb54a34fe8d6f49ae7fb6cece96794d5
Eric Sigurdson <eric@ericsigurdson.com>
2025-04-26 12:50:57 -0400

    Include clojure cider repl integration, and enable to QOL stuff for programming

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,8 +4,15 @@
 (scroll-bar-mode -1)
 (global-display-line-numbers-mode 1)
 
-;; Packages
+;; Coding QOL stuff
+(show-paren-mode 1)
+(setq scroll-margin 5
+      scroll-conservatively 100)
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
 
+;; Packages
 (require 'package)
 
 (setq package-archives
@@ -22,10 +29,15 @@
 
 (require 'use-package)
 
+;; Emacs git interface
 (use-package magit
   :ensure t)
 
+;; Pretty colours
 (use-package gruvbox-theme
   :ensure t
   :config (load-theme 'gruvbox-dark-soft t))
 
+;; Clojure REPL integration
+(use-package cider
+  :ensure t)
