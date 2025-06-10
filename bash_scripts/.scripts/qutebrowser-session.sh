31ff01e60b8a197ea3f33905627430fa0becbf3a
Eric Sigurdson <eric@ericsigurdson.com>
2025-06-10 11:29:20 -0400

    feat(browser-session): script to open browser sessions via rofi menu

--- bash_scripts/.scripts/qutebrowser-session.sh
+++ bash_scripts/.scripts/qutebrowser-session.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-SESSION=$(printf "work\npersonal\nresearch" | dmenu -p "Qute session:")
+SESSION=$(printf "raw\nbd-admin\nbd-user\npersonal" | rofi -dmenu -p "Qute session:")
 [ -z "$SESSION" ] && exit
 
 BASEDIR="$HOME/.config/qutebrowser/sessions/$SESSION"
 mkdir -p "$BASEDIR"
+mkdir -p "$BASEDIR/data/"
 
-exec qutebrowser --basedir "$BASEDIR"
+ln -s "~/.config/qutebrowser/userscripts/"
+
+exec qutebrowser \
+    --basedir "$BASEDIR" \
+    --config-py "$HOME/.config/qutebrowser/config.py" \
+    --set window.title_format "{perc}$SESSION{title_sep}{current_title}" "$URL"
+#exec qutebrowser --basedir "$BASEDIR"
