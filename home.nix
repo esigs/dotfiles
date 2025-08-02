5c8e90215351e97137e57a4acbb2d4e9ba0d2579
Eric Sigurdson <eric.sigurdson@example.com>
2025-08-01 20:46:42 -0400

    chore: clean up readability

--- home.nix
+++ home.nix
@@ -1,29 +1,27 @@
 { config, pkgs, inputs, ... }:
 {
-	home.stateVersion = "25.05"; # required for home-manager
-	home.sessionVariables.EDITOR = "nvim";
-	imports = [ inputs.nixvim.homeManagerModules.nixvim ];
-	programs.nixvim = {
-		enable = true;
+  home.stateVersion = "25.05"; # required for home-manager
+  home.sessionVariables.EDITOR = "nvim";
+  imports = [ inputs.nixvim.homeManagerModules.nixvim ];
+  programs.nixvim = {
+    enable = true;
 
-		colorschemes.catppuccin.enable = true;
-		plugins.lualine.enable = true;
-		plugins.treesitter.enable = true;
-		plugins.nix.enable = true;
+    colorschemes.catppuccin.enable = true;
+    plugins.lualine.enable = true;
+    plugins.treesitter.enable = true;
+    plugins.nix.enable = true;
 
 
-                clipboard = {
-                  register = "unnamedplus"; # Uses system clipboard
-                  providers.xclip.enable = true;  # OR:
-                  #providers.xsel.enable = true;   # one of these is enough
-                  #providers.wl-copy.enable = true; # for Wayland
-                };
+    clipboard = {
+      register = "unnamedplus"; # Uses system clipboard
+      providers.xclip.enable = true; 
+      };
 
-              };
+    };
 
-              programs.git = {
-                enable = true;
-                userName = "Eric Sigurdson";
-                userEmail = "eric.sigurdson@example.com";
-              };
-            }
+    programs.git = {
+      enable = true;
+      userName = "Eric Sigurdson";
+      userEmail = "eric.sigurdson@example.com";
+    };
+  }
